ba57e948142f391145aaf3d2f58d59685da0fd92
Bas Dingemans <BasD92@gmail.com>
2017-03-01 20:04:54 +0100

    Delete local db information

--- dbConnection/connect.php
+++ dbConnection/connect.php
@@ -1,10 +1,5 @@
 <?php
-$dbHost = 'localhost';
-$dbUser = 'root';
+$dbHost = '';
+$dbUser = '';
 $dbPass = '';
-$dbDatabase = 'betmatch';
-
-//$dbHost = 'db.wedplek.nl';
-//$dbUser = 'md348476db344836';
-//$dbPass = 'OPlI7mEc';
-//$dbDatabase = 'md348476db344836';
+$dbDatabase = '';
